e3143076c68f51d37b253d6c1f082529a629399d
babbi <babitha.addala@gmail.com>
2023-06-02 12:15:05 -0500

    Test case updated

--- src/test/java/com/retailer/rewards/charter/service/RewardsServiceImplTest.java
+++ src/test/java/com/retailer/rewards/charter/service/RewardsServiceImplTest.java
@@ -11,9 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.sql.Timestamp;
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.time.temporal.TemporalAdjusters;
 import java.util.Collections;
 
@@ -31,50 +29,40 @@ public class RewardsServiceImplTest {
     @Test
     public void testGetRewardsByCustomerId() {
 
-       /* LocalDate lastMonth = LocalDate.now().minusMonths(1).with(TemporalAdjusters.firstDayOfMonth());
-        LocalDate lastSecondMonth = LocalDate.now().minusMonths(2).with(TemporalAdjusters.firstDayOfMonth());
-        LocalDate lastThirdMonth = LocalDate.now().minusMonths(3).with(TemporalAdjusters.firstDayOfMonth());
-        Timestamp timestamp1 = Timestamp.valueOf(lastMonth.atTime(LocalTime.MIDNIGHT));
-        Timestamp timestamp2 = Timestamp.valueOf(lastMonth.atTime(LocalTime.MIDNIGHT));
-        Timestamp timestamp3 = Timestamp.valueOf(lastMonth.atTime(LocalTime.MIDNIGHT));
-
         Transaction transaction3 = new Transaction();
         transaction3.setCustomerId(1001L);
         transaction3.setTransactionId(3L);
-        transaction3.setTransactionDate(timestamp1);
+        transaction3.setTransactionDate(LocalDate.now().minusMonths(3).with(TemporalAdjusters.firstDayOfMonth()));
         transaction3.setTransactionAmount(120);
 
         Transaction transaction2 = new Transaction();
         transaction2.setCustomerId(1001L);
         transaction2.setTransactionId(2L);
-        transaction2.setTransactionDate(timestamp2);
+        transaction2.setTransactionDate(LocalDate.now().minusMonths(2).with(TemporalAdjusters.firstDayOfMonth()));
         transaction2.setTransactionAmount(75);
 
         Transaction transaction1 = new Transaction();
         transaction1.setCustomerId(1001L);
         transaction1.setTransactionId(1L);
-        transaction1.setTransactionDate(timestamp3);
+        transaction1.setTransactionDate(LocalDate.now().minusMonths(1).with(TemporalAdjusters.firstDayOfMonth()));
         transaction1.setTransactionAmount(140);
 
 
-
-        when(transactionRepository.findAllByCustomerIdAndTransactionDate
-                (1001L, Timestamp.valueOf(lastMonth.atTime(LocalTime.MIDNIGHT))))
+        when(transactionRepository.findAllByCustomerIdAndTransactionDateBetween
+                (1001L, LocalDate.now().minusMonths(1).with(TemporalAdjusters.firstDayOfMonth()), LocalDate.now().minusMonths(1).with(TemporalAdjusters.lastDayOfMonth())))
                 .thenReturn(Collections.singletonList(transaction1));
 
-        when(transactionRepository.findAllByCustomerIdAndTransactionDate
-                (1001L, Timestamp.valueOf(lastSecondMonth.atTime(LocalTime.MIDNIGHT))))
+        when(transactionRepository.findAllByCustomerIdAndTransactionDateBetween
+                (1001L, LocalDate.now().minusMonths(2).with(TemporalAdjusters.firstDayOfMonth()), LocalDate.now().minusMonths(2).with(TemporalAdjusters.lastDayOfMonth())))
                 .thenReturn(Collections.singletonList(transaction2));
 
-        when(transactionRepository.findAllByCustomerIdAndTransactionDate
-                (1001L, Timestamp.valueOf(lastThirdMonth.atTime(LocalTime.MIDNIGHT))))
+        when(transactionRepository.findAllByCustomerIdAndTransactionDateBetween
+                (1001L, LocalDate.now().minusMonths(3).with(TemporalAdjusters.firstDayOfMonth()), LocalDate.now().minusMonths(3).with(TemporalAdjusters.lastDayOfMonth())))
                 .thenReturn(Collections.singletonList(transaction3));
 
         Rewards response = rewardsService.getRewardsByCustomerId(1001L);
         Assertions.assertNotNull(response);
         Assertions.assertEquals(1001, response.getCustomerId());
-        Assertions.assertEquals(245, response.getTotalRewards());
-
-        */
+        Assertions.assertEquals(245.0, response.getTotalRewards());
     }
 }
